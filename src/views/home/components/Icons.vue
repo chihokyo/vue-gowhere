6b5504c64bcb08b44a822dbce95d1cfd95d0ed3c
chihokyo <chihoukyo@gmail.com>
2020-08-14 17:10:13 +0900

    【Step1】修改icon页面轮播bug

--- src/views/home/components/Icons.vue
+++ src/views/home/components/Icons.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="icons">
-    <swiper ref="mySwiper">
+    <swiper ref="mySwiper" :options="swiperOptions">
       <!-- 计算总共有多少页 -->
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <!-- 针对这一页开始循环 -->
         <div class="icon"
              v-for="(item,index) of page"
-             :key="index">
+             :key="index"
+        >
           <div class="icon-img">
             <img class="icon-img-content"
                 :src="item.url"
-                :alt="item.desc">
+                :alt="item.desc"
+            >
           </div>
           <p class="icon-desc">{{item.desc}}</p>
         </div>
@@ -24,6 +26,10 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        autoplay: false
+      },
       iconList: [
         {
           id: '0001',
@@ -103,33 +109,35 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    position: relative
-    overflow: hidden
-    float: left
-    width: 25%
-    height: 0
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      box-sizing: border-box
-      padding: .1rem
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      left: 0
-      right: 0
-      bottom: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $darkTextColor
-      ellipsis()
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
 </style>
