72edee61d20bbc9ff8c157a558be0e0c26293f34
chihokyo <chihoukyo@gmail.com>
2020-08-18 00:14:23 +0900

    【Step1】组件套用

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,6 +2,9 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
@@ -9,12 +12,45 @@
 
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票1',
+          children: [
+            {
+              title: '成人成人联票1-1',
+              children: [
+                {
+                  title: '成人成人成人联票1-1-1'
+                }
+              ]
+            },
+            {
+              title: '成人成人联票2'
+            }
+          ]
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
