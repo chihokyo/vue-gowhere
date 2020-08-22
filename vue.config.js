bfa92d600306cba42092742da4596ab827101ac8
chihokyo <chihoukyo@gmail.com>
2020-08-22 16:51:03 +0900

    【Step1】修改axios转发路径

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/static/mock',
+        target: 'https://github.com/chihokyo/vue-gowhere/static/mock',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
